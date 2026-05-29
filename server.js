6368a0e4a610e0ed840880d5e5f036861b7cd652
ezq-l <eze30082007@gmail.com>
2026-05-29 00:26:34 -0300

    commit correccion4
    correccion del server.js para que muestre en el render el chat con el bot

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'chat.html'));
+});
+
 // =============================================
 //  CONEXIÓN A POSTGRESQL
 // =============================================
